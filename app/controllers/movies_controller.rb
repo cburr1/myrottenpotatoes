ad0d4ea59aa2c20027bef166438127f1b1906e0f
Cory Burr <cburr1@uncc.edu>
2016-10-21 18:09:22 +0000

    destroy functionality added.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -26,12 +26,20 @@ class MoviesController < ApplicationController
   end
   
   def update
-    @movie = Movie.find params[:id]
-    @movie.update_attribute!(params[:movie])
+    @movie = Movie.find(params[:id])
+    @movie.update_attributes!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
-
+  
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie #{@movie.title} deleted."
+    redirect_to movies_path
+  end
+  
+  
   private
   def user_params
     params.require(:movie)
